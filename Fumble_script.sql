32afdefd1811d265f96776028b5829ea2f717545
httpsrenan <renan.pereira@sptech.school>
2025-05-18 22:10:22 -0300

    BD arrumado

--- Fumble_script.sql
+++ Fumble_script.sql
@@ -29,16 +29,23 @@ create table usuarios(
 id    int primary key auto_increment,
 email varchar(100) not null unique,
 nome varchar(50) not null,
-senha varchar(200),
-time  int,
-constraint fk_time foreign key (time) references times(id)
+senha varchar(200)
 );
 
+create table favoritar(
+fk_time int,
+fk_usuario int,
+primary key (fk_time, fk_usuario),
+constraint fk_time foreign key (fk_time) references times(id),
+constraint fk_usuario foreign key (fk_usuario) references usuarios(id)
+);
+
+-- Inserindo conferências
 INSERT INTO conferencia (conferencia, sigla) VALUES
 ('Conferência Americana de Futebol', 'AFC'),
 ('Conferência Nacional de Futebol', 'NFC');
 
-
+-- Inserindo divisões
 INSERT INTO divisao (divisao, sigla, fk_conferencia) VALUES
 ('Divisão Leste da AFC', 'AFCE', 1),
 ('Divisão Norte da AFC', 'AFCN', 1),
@@ -49,7 +56,7 @@ INSERT INTO divisao (divisao, sigla, fk_conferencia) VALUES
 ('Divisão Sul da NFC', 'NFCS', 2),
 ('Divisão Oeste da NFC', 'NFCW', 2);
 
-
+-- Inserindo times
 INSERT INTO times (nome, apelido, estado, fk_divisao) VALUES
 ('Buffalo Bills', 'Bills', 'Nova York', 1),
 ('Miami Dolphins', 'Dolphins', 'Flórida', 1),
@@ -84,24 +91,62 @@ INSERT INTO times (nome, apelido, estado, fk_divisao) VALUES
 ('San Francisco 49ers', '49ers', 'Califórnia', 8),
 ('Seattle Seahawks', 'Seahawks', 'Washington', 8);
 
-INSERT INTO usuarios (email, nome, senha, time) VALUES
-('tom.brady@example.com', 'Tom Brady', '123456', 31),
-('patrick.mahomes@example.com', 'Patrick Mahomes', '123456', 17),
-('joe.burrow@example.com', 'Joe Burrow', '123456', 7), 
-('justin.jefferson@example.com', 'Justin Jefferson', '123456', 21), 
-('jalen.hurts@example.com', 'Jalen Hurts', '123456', 27), 
-('lamar.jackson@example.com', 'Lamar Jackson', '123456', 3),
-('josh.allen@example.com', 'Josh Allen', '123456', 4), 
-('aaron.donald@example.com', 'Aaron Donald', '123456', 19), 
-('tyreek.hill@example.com', 'Tyreek Hill', '123456', 20),
-('miles.garrett@example.com', 'Myles Garrett', '123456', 8), 
-('davante.adams@example.com', 'Davante Adams', '123456', 17), 
-('cooper.kupp@example.com', 'Cooper Kupp', '123456', 19), 
-('dk.metcalf@example.com', 'DK Metcalf', '123456', 29), 
-('trevor.lawrence@example.com', 'Trevor Lawrence', '123456', 15), 
-('nick.bosa@example.com', 'Nick Bosa', '123456', 28),
-('micah.parsons@example.com', 'Micah Parsons', '123456', 9),
-('derrick.henry@example.com', 'Derrick Henry', '123456', 32), 
-('deebo.samuels@example.com', 'Deebo Samuel', '123456', 28),
-('kirk.cousins@example.com', 'Kirk Cousins', '123456', 21),
-('chris.godwin@example.com', 'Chris Godwin', '123456', 31);
+-- Inserindo usuários 
+INSERT INTO usuarios (email, nome, senha) VALUES
+('tom.brady@example.com', 'Tom Brady', '123456'),
+('patrick.mahomes@example.com', 'Patrick Mahomes', '123456'),
+('joe.burrow@example.com', 'Joe Burrow', '123456'), 
+('justin.jefferson@example.com', 'Justin Jefferson', '123456'), 
+('jalen.hurts@example.com', 'Jalen Hurts', '123456'), 
+('lamar.jackson@example.com', 'Lamar Jackson', '123456'),
+('josh.allen@example.com', 'Josh Allen', '123456'), 
+('aaron.donald@example.com', 'Aaron Donald', '123456'), 
+('tyreek.hill@example.com', 'Tyreek Hill', '123456'),
+('miles.garrett@example.com', 'Myles Garrett', '123456'), 
+('davante.adams@example.com', 'Davante Adams', '123456'), 
+('cooper.kupp@example.com', 'Cooper Kupp', '123456'), 
+('dk.metcalf@example.com', 'DK Metcalf', '123456'), 
+('trevor.lawrence@example.com', 'Trevor Lawrence', '123456'), 
+('nick.bosa@example.com', 'Nick Bosa', '123456'),
+('micah.parsons@example.com', 'Micah Parsons', '123456'),
+('derrick.henry@example.com', 'Derrick Henry', '123456'), 
+('deebo.samuels@example.com', 'Deebo Samuel', '123456'),
+('kirk.cousins@example.com', 'Kirk Cousins', '123456'),
+('chris.godwin@example.com', 'Chris Godwin', '123456');
+
+-- Inserindo favoritos 
+INSERT INTO favoritar (fk_usuario, fk_time) VALUES
+(1, 3),   
+(1, 30),  
+(2, 14),  
+(2, 17), 
+(3, 6),  
+(3, 7),  
+(4, 24),  
+(4, 19),  
+(5, 19),  
+(5, 20),  
+(6, 5),   
+(6, 2),   
+(7, 1),   
+(7, 4),  
+(8, 30),  
+(8, 29),  
+(9, 20),  
+(9, 2),   
+(10, 8),  
+(10, 6),  
+(11, 14), 
+(11, 15), 
+(12, 29), 
+(12, 30), 
+(13, 32), 
+(13, 28), 
+(14, 11), 
+(14, 3),  
+(15, 28), 
+(15, 30); 
+
+select u.*, t.* from usuarios u 
+inner join favoritar on u.id = fk_usuario
+inner join times t on t.id = fk_time;
