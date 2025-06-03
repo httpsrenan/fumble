fda4229ba607524ba279511eeb072f4c6a3ddddd
httpsrenan <renan.pereira@sptech.school>
2025-06-03 14:45:20 -0300

    melhorias

--- site/src/models/dashboardModel.js
+++ site/src/models/dashboardModel.js
@@ -17,9 +17,9 @@ function buscarTop10MaisFavoritados() {
     var instrucaoSql = `
         select t.nome, count(f.fk_time) as total_favoritos
         from favoritar f
-        inner join times t ON f.fk_time = t.id
+        inner join times t on f.fk_time = t.id
         group by f.fk_time
-        order by total_favoritos DESC
+        order by total_favoritos desc
         limit 10;
     `;
     return database.executar(instrucaoSql);
@@ -30,9 +30,9 @@ function buscarTop10MenosFavoritados() {
     var instrucaoSql = `
         select t.nome, count(f.fk_time) as total_favoritos
         from times t
-        left join favoritar f ON f.fk_time = t.id
+        left join favoritar f on f.fk_time = t.id
         group by t.id
-        order by total_favoritos ASC
+        order by total_favoritos asc
         limit 10;
     `;
     return database.executar(instrucaoSql);
@@ -43,9 +43,9 @@ function buscarTop5UsuariosMaisFavoritaram() {
     var instrucaoSql = `
         select u.nome, count(f.fk_usuario) as total_favoritos
         from favoritar f
-        inner join usuarios u ON f.fk_usuario = u.id
+        inner join usuarios u on f.fk_usuario = u.id
         group by f.fk_usuario
-        order by total_favoritos DESC
+        order by total_favoritos desc
         limit 5;
     `;
     return database.executar(instrucaoSql);
@@ -56,9 +56,9 @@ function buscarFavoritosPorConferencia() {
     var instrucaoSql = `
         select c.conferencia, count(f.fk_time) as total_favoritos
         from favoritar f
-        inner join times t ON f.fk_time = t.id
-        inner join divisao d ON t.fk_divisao = d.id
-        inner join conferencia c ON d.fk_conferencia = c.id
+        inner join times t on f.fk_time = t.id
+        inner join divisao d on t.fk_divisao = d.id
+        inner join conferencia c on d.fk_conferencia = c.id
         group by c.id;
     `;
     return database.executar(instrucaoSql);
--- site/src/models/favoritoModel.js
+++ site/src/models/favoritoModel.js
@@ -2,22 +2,22 @@ var database = require("../database/config");
 
 function inserirFavorito(idUsuario, idTime) {
   var instrucaoSql = `
-    INSERT INTO favoritar (fk_usuario, fk_time) VALUES (${idUsuario}, ${idTime});
+    insert into favoritar (fk_usuario, fk_time) vslues (${idUsuario}, ${idTime});
   `;
   return database.executar(instrucaoSql);
 }
 
 function removerFavorito(idUsuario, idTime) {
   var instrucaoSql = `
-    DELETE FROM favoritar WHERE fk_usuario = ${idUsuario} AND fk_time = ${idTime};
+    delete from favoritar where fk_usuario = ${idUsuario} and fk_time = ${idTime};
   `;
   return database.executar(instrucaoSql);
 }
 
 function verificarFavorito(idUsuario, idTime) {
     var instrucaoSql = `
-        SELECT * FROM favoritar
-        WHERE fk_usuario = ${idUsuario} AND fk_time = ${idTime};
+        select * from favoritar
+        where fk_usuario = ${idUsuario} and fk_time = ${idTime};
     `;
     return database.executar(instrucaoSql);
 }
